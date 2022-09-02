50efff6107ef86083cb53255dca58878df981245
LEONARDO RODRIGUES DE RIBAMAR DA SILVA <leonardo.rrsilva@CAS474651MAC09.local>
2022-09-01 21:24:58 -0300

    Subindo projeto

---
+++ app/src/main/java/com/ossmurfy/bravo4fun/MainMenu.kt
@@ -0,0 +1,24 @@
+package com.ossmurfy.bravo4fun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
+class MainMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+
+        var resultado: Number = 0
+        val texto: TextView = findViewById(R.id.number)
+
+        fun mudaNumero() {
+            resultado = 10;
+            texto.toString().toInt()
+            texto.text = resultado.toString()
+
+            
+        }
+    }
+}
