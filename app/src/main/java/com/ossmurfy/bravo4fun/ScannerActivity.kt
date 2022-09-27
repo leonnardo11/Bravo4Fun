e1162f1ed4beddcd8ac311da17e445e63a01af74
MaKoTo <matheus.mkodama@gmail.com>
2022-09-27 16:16:09 -0300

    tela de Scanner finalizada

---
+++ app/src/main/java/com/ossmurfy/bravo4fun/ScannerActivity.kt
@@ -0,0 +1,11 @@
+package com.ossmurfy.bravo4fun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScannerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanner)
+    }
+}
