68fd726808a268b59b5ef96bb65cbd1ebc85baca
gustavocquinto <gutocross.kx@gmail.com>
2022-09-29 20:08:49 -0300

    BottomNav v2 - Botão na home "ver mais" funcionando
    Botão "sair" no perfil de usuário retorna a página de login

--- app/src/main/java/com/ossmurfy/bravo4fun/ProfileActivity.kt
+++ app/src/main/java/com/ossmurfy/bravo4fun/ProfileActivity.kt
@@ -1,11 +1,23 @@
 package com.ossmurfy.bravo4fun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ossmurfy.bravo4fun.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sairButton.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
