88e4ae8e47c9fba74aa7462149d081bebc466b7b
gustavo.cquinto <gutocross.fx@gmail.com>
2022-11-10 20:55:26 -0300

    Funcoes events fragment

--- app/src/main/java/com/ossmurfy/bravo4fun/fragments/EventsFragment.kt
+++ app/src/main/java/com/ossmurfy/bravo4fun/fragments/EventsFragment.kt
@@ -6,6 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ossmurfy.bravo4fun.R
+import com.ossmurfy.bravo4fun.model.Produto
+import com.ossmurfy.bravo4fun.service.API
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class EventsFragment : Fragment() {
@@ -17,5 +22,78 @@ class EventsFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_events, container, false)
     }
+
+    fun atualizarProdutos() {
+
+        //Callback acionado quando a execução da API concluir
+        val callback = object : Callback<List<Produto>> {
+
+            //Chamada quando o endpoint responder
+            override fun onResponse(call: Call<List<Produto>>, response: Response<List<Produto>>) {
+
+                //desabilitarCarregamento()
+
+                if (response.isSuccessful) {
+                    val listaProdutos = response.body()
+                    atualizarUI(listaProdutos)
+                }
+                else {
+                    //val error = response.errorBody().toString()
+                    //Snackbar.make(binding.container, "Não é possível autualizar os produtos",
+                        //Snackbar.LENGTH_LONG).show()
+
+                    //Log.e("ERROR", response.errorBody().toString())
+                }
+            }
+
+            //Chamada caso aconteça algum problema e não seja possível bater no endpoint
+            //Ou a resposta seja incompatível
+            override fun onFailure(call: Call<List<Produto>>, t: Throwable) {
+                //desabilitarCarregamento()
+
+                //Snackbar.make(binding.container, "Não foi possível se conectar ao servidor",
+                    //Snackbar.LENGTH_LONG).show()
+
+                //Log.e("ERROR", "Falha ao executar serviço", t)
+            }
+        }
+
+        //Faz a chamada a API
+        API().produto.listAll().enqueue(callback)
+
+        //Chama uma função para habilitar o carregamento
+        //habilitarCarregamento()
+    }
+
+    fun atualizarUI(lista: List<Produto>?) {
+        //Limpa a lista de itens
+        binding.container.removeAllViews()
+
+        //Itera pela lista de respostas
+        lista?.forEach {
+            //ELEMENTOS DINÂMICOS
+            //Cria um cartão dinamicamente
+            val cardBinding = CardItemBinding.inflate(layoutInflater)
+
+            //Configura os itens do cartão com os valores do
+            //item do array
+            cardBinding.textTitulo.text = it.nomeProduto
+            cardBinding.textDesc.text = it.descProduto
+
+            //Solicita o carregamento da imagem
+            Picasso.get().load(
+                "https://oficinacordova.azurewebsites.net/android/rest/produto/image/${it.idProduto}"
+            ).placeholder(R.drawable.no_image).error(R.drawable.no_image).into(cardBinding.imagem)
+
+            cardBinding.root.setOnClickListener { cartao ->
+                val frag = DetalheProdutoFragment(it.idProduto)
+                activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.container, frag)?.addToBackStack("Detalhe do Produto")?.commit()
+            }
+
+            //Adiciona o cartão no container para que apareça na tela
+            binding.container.addView(cardBinding.root)
+        }
+    }
+
 
 }
